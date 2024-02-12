efd829cc3af7c69b1a88239051b10bf642c68903
firreee <143442624+firreee@users.noreply.github.com>
2024-02-12 10:32:42 -0600

    Update P5.py

--- P5.py
+++ P5.py
@@ -25,3 +25,36 @@ while table:
                 print(f"{num / i} = {num} / {i}")
             else:
                 print("Cannot divide by zero.")
+
+table = True
+
+while table:
+  action = input("Select table code: ").upper()
+  num = float(input("Enter number for table: "))
+
+  if action == 'A':
+    print("Add")
+    for i in range(1, 11):
+      print(f"{num + i} = {num} + {i}")
+  elif action == 'S':
+    print("Subtract")
+    for i in range(1, 11):
+      print(f"{num - i} = {num} - {i}")
+  elif action == 'M':
+    print("Multiply")
+    for i in range(1, 11):
+      print(f"{num * i} = {num} * {i}")
+  elif action == 'D':
+    print("Divide")
+    for i in range(1, 11):
+      if i != 0:
+        print(f"{num / i} = {num} / {i}")
+      else:
+        print("Cannot divide by zero.")
+
+  # Ask for next iteration
+  answer = input("Generate another table? (y/n): ").lower()
+  table = answer == "y"
+
+print("Goodbye!")
+
